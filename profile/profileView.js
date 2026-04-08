fc27aa7012c0cb58468dfce166b12bd8fdf2de27
andreasiversen98 <andreasiversen98@protonmail.com>
2026-04-08 14:20:36 +0200

    Update profileView.js

--- profile/profileView.js
+++ profile/profileView.js
@@ -2,8 +2,35 @@ updateProfileView();
 
 function updateProfileView() {
   const app = document.getElementById("app");
+  const currentUser = model.users[0];
+  const userGoals = model.goals[0];
 
   app.innerHTML = /*HTML*/ `
-        
+       <main>
+            <header>
+                <h1>${currentUser.displayName}</h1>
+                <button>Rediger Mål</button>
+            </header>
+
+            <section>
+                <h2>Mål for søvn:</h2>
+            </section>
+            
+            <section>
+                <span>Mengde søvn:</span>
+                <span>${userGoals.amountSleptGoal} Timer</span>
+            </section>
+
+            <section>
+                <span>Fra klokken:</span>
+                <span>${userGoals.bedTimeGoal}</span>
+                <span>til klokken:</span>
+                <span>${userGoals.wakeUpGoal}</span>
+            </section>
+
+            <footer>
+                <button>Lagre</button>
+            </footer>
+        </main>
     `;
 }
