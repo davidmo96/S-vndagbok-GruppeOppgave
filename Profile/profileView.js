beb954e08135a594f6de9b7619ec03867ce0eaac
andreasiversen98 <andreasiversen98@protonmail.com>
2026-04-10 12:17:25 +0200

    Added css

--- Profile/profileView.js
+++ Profile/profileView.js
@@ -6,32 +6,32 @@ function updateProfileView() {
   const userGoals = model.goals[0];
 
   app.innerHTML = /*HTML*/ `
-       <main class=profile-page>
-            <header>
-                <h1>${currentUser.displayName}</h1>
-                <button onclick="editGoals()">Rediger Mål</button>
-            </header>
+    <main class="profile-page">
+        <header>
+            <h1>${currentUser.displayName}</h1>
+            <button onclick="editGoals()">Rediger Mål</button>
+        </header>
 
-            <section>
-                <h2>Mål for søvn:</h2>
-            </section>
-            
-            <section>
-                <span>Mengde søvn:</span>
-                <span>${userGoals.amountSleptHours} Timer</span>
-                <span>${userGoals.amountSleptMinutes} Minutter</span>
-            </section>
+        <section>
+            <h2>Mål for søvn:</h2>
+        </section>
+        
+        <section>
+            <span>Mengde søvn:</span>
+            <span class="goal-box">${userGoals.amountSleptHours} Timer</span>
+            <span class="goal-box">${userGoals.amountSleptMinutes} Minutter</span>
+        </section>
 
-            <section>
-                <span>Fra klokken:</span>
-                <span>${userGoals.bedTimeGoal}</span>
-                <span>til klokken:</span>
-                <span>${userGoals.wakeUpGoal}</span>
-            </section>
+        <section>
+            <span>Fra klokken:</span>
+            <span class="goal-box">${userGoals.bedTimeGoal}</span>
+            <span>til klokken:</span>
+            <span class="goal-box">${userGoals.wakeUpGoal}</span>
+        </section>
 
-            <footer>
-                <button onclick="saveGoals()">Lagre</button>
-            </footer>
-        </main>
-    `;
+        <footer>
+            <button onclick="saveGoals()">Lagre</button>
+        </footer>
+    </main>
+`;
 }
