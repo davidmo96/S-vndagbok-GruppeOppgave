93aff50a006777fb30762abe77728d28d3d62e45
celsve-cmd <celsve@gmail.com>
2026-04-17 09:54:10 +0200

    -

--- viewcontroller.js
+++ viewcontroller.js
@@ -9,8 +9,20 @@ function updateView() {
   if (model.app.currentPage === "logView") logView();
   if (model.app.currentPage === "overViewPage") showOverView();
   if (model.app.currentPage === "profileView") updateProfileView();
-  
+
   if (!model.app.currentUser) model.app.currentPage = "login";
   if (model.app.currentPage === "login") loginView();
 
+}
+
+
+function createDropDownButton() {
+  return /*HTML*/`
+    <div class="dropdown-content">
+      <button onclick="changePage('logView')" class="dropdownContentButtons">Logg</button>
+      <button onclick="changePage('overViewPage')" class="dropdownContentButtons">Oversikt</button>
+      <button onclick="changePage('profileView')" class="dropdownContentButtons">Profil</button>
+      <button onclick="logOut()" class="dropdownContentButtons">Logg ut</button>
+    </div>
+  `;
 }
